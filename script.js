9ed37642aa773cb4a3cba036e49913602ccac620
THACHER <sophie.thacher@gmail.com>
2016-03-07 15:28:20 -0800

    everything working pre-grid

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 'use strict';
 
 $( document ).ready(function() {
-
-  
+// $('#fightPirates').on('click', function(){
+//   console.log('clicked');
+// });
 });
+
+
+
+//Run Form Validator
+// $(function () {
+//   if(document.location.search) {
+//     var queryString = document.location.search.replace('?', '');
+//     var pairs = queryString.split('&').map(function (pair) {
+//       return pair.split('=');
+//     });
+//
+//     $('.query-params').show();
+//     var $tbody = $('.query-params tbody');
+//
+//     pairs.forEach(function (pair) {
+//       var $tr = $('<tr>');
+//       $tr.append($('<td>').html(pair[0]));
+//       $tr.append($('<td>').html(pair[1]));
+//       $tbody.append($tr);
+//     });
+//   }
+// });
