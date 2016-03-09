05102f740c2f92b91b2f2f9c1a84ce43fd2df5b4
THACHER <sophie.thacher@gmail.com>
2016-03-08 19:26:51 -0800

    clean up and small changes to grid

--- script.js
+++ script.js
@@ -9,18 +9,21 @@ $('.ocean').on('click', function(event){
 });
 
 var atlanticOcean = document.getElementsByClassName('atlanticOcean')[0];
-var wrapper = document.createElement('section');
-atlanticOcean.appendChild(wrapper);
+var set = document.createElement('section');
+atlanticOcean.appendChild(set);
 
-  for (var i = 0; i < 10; i++) {
-    var wave = document.createElement('div');
-    // wave.className = 'wave';
-    wrapper.appendChild(wave);
-    wave.style.clear = 'both';
-    for (var j = 0; j < 10; j++) {
-      var gameSquare = document.createElement('div');
-      wave.appendChild(gameSquare);
-      gameSquare.className = 'gameSquare';
-      gameSquare.id = i + " " + j;
-    }
+for (var i = 0; i < 10; i++) {
+  var wave = document.createElement('div');
+  set.appendChild(wave);
+  wave.style.clear = 'both';
+  for (var j = 0; j < 10; j++) {
+    var gameSquare = document.createElement('div');
+    wave.appendChild(gameSquare);
+    gameSquare.className = 'gameSquare';
+    gameSquare.id = j + " " + i;
   }
+}
+
+// var ship = {};
+// document.createElement.
+// ship.className = '.ship';
