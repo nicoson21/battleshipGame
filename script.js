4630e9d0393f75d797addc91439f42ec2d9f87e2
THACHER <sophie.thacher@gmail.com>
2016-03-08 16:03:33 -0800

    move from bootstrap grid to JS created grid

--- script.js
+++ script.js
@@ -6,10 +6,38 @@ $('.ocean').on('click', function(event){
   event.target.style.background = 'green';
 
 });
-
-
 });
+//
+// var test = document.getElementsByClassName('x0 y0');
+// test.style.backgroundColor = 'blue';
+//
+var atlanticOcean = document.getElementsByClassName('atlanticOcean')[0];
+var wrapper = document.createElement('section');
+atlanticOcean.appendChild(wrapper);
 
+  for (var i = 0; i < 10; i++) {
+    var wave = document.createElement('div');
+    // wave.className = 'wave';
+    wrapper.appendChild(wave);
+    wave.style.clear = 'both';
+    for (var j = 0; j < 10; j++) {
+      var gameSquare = document.createElement('div');
+      wave.appendChild(gameSquare);
+      gameSquare.className = 'gameSquare';
+      gameSquare.id = i + " " + j;
+    }
+  }
+
+//   for(var i = 0; i < 12; i++){
+//     gameSquare.id = i;
+//     for(var j = 0; j < 12; j++){
+//       gameSquare.id += j;
+//       var paletteSquare = document.createElement('div');
+//       document.getElementsByTagName('section')[1].appendChild(paletteSquare);
+//       gameSquare.className = 'gameSquare';
+//
+//     }
+// }
 
 
 //Run Form Validator
