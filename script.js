7b4fd1b56295d6b66a8b1bbe43b98aa8fe8466f2
THACHER <sophie.thacher@gmail.com>
2016-03-08 16:05:14 -0800

    clean up post- removal of bootstrap grid

--- script.js
+++ script.js
@@ -7,10 +7,7 @@ $('.ocean').on('click', function(event){
 
 });
 });
-//
-// var test = document.getElementsByClassName('x0 y0');
-// test.style.backgroundColor = 'blue';
-//
+
 var atlanticOcean = document.getElementsByClassName('atlanticOcean')[0];
 var wrapper = document.createElement('section');
 atlanticOcean.appendChild(wrapper);
@@ -27,35 +24,3 @@ atlanticOcean.appendChild(wrapper);
       gameSquare.id = i + " " + j;
     }
   }
-
-//   for(var i = 0; i < 12; i++){
-//     gameSquare.id = i;
-//     for(var j = 0; j < 12; j++){
-//       gameSquare.id += j;
-//       var paletteSquare = document.createElement('div');
-//       document.getElementsByTagName('section')[1].appendChild(paletteSquare);
-//       gameSquare.className = 'gameSquare';
-//
-//     }
-// }
-
-
-//Run Form Validator
-// $(function () {
-//   if(document.location.search) {
-//     var queryString = document.location.search.replace('?', '');
-//     var pairs = queryString.split('&').map(function (pair) {
-//       return pair.split('=');
-//     });
-//
-//     $('.query-params').show();
-//     var $tbody = $('.query-params tbody');
-//
-//     pairs.forEach(function (pair) {
-//       var $tr = $('<tr>');
-//       $tr.append($('<td>').html(pair[0]));
-//       $tr.append($('<td>').html(pair[1]));
-//       $tbody.append($tr);
-//     });
-//   }
-// });
