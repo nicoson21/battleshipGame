036648327c3e0506366e64ae210fef18d5bafba9
THACHER <sophie.thacher@gmail.com>
2016-03-09 17:44:54 -0800

    it fucking works

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
 'use strict';
-
-$( document ).ready(function() {
-$('.gameSquare').on('click', function(event){
-  console.log(this);
-  event.target.style.background = 'green';
-});
-});
-
+var shipLength;
+var shipPlacement = new Array();
 var atlanticOcean = document.getElementsByClassName('atlanticOcean')[0];
 var set = document.createElement('section');
+
+$( document ).ready(function() {
+  $('.gameSquare').on('click', function(event){
+      // $(event.target).toggle();
+
+    console.log(event.target.id.replace('square', '').split(''));
+    var a = event.target.id.charAt(6);
+    var b = event.target.id.charAt(7);
+
+    if((shipPlacement[a][b]) === 1){
+      console.log('hit');
+      event.target.style.background = 'red';
+    } else {
+      console.log('miss');
+      event.target.style.background = 'white';
+    }
+    // console.log(shipPlacement[a][b]);
+
+  });
+});
+
 atlanticOcean.appendChild(set);
-var shipPlacement = new Array();
 
 for (var i = 0; i < 10; i++) {
   var wave = document.createElement('div');
@@ -20,12 +34,17 @@ for (var i = 0; i < 10; i++) {
   for (var j = 0; j < 10; j++) {
     var gameSquare = document.createElement('div');
     wave.appendChild(gameSquare);
+
+    // var gameSquareCover = document.createElement('div');
+    // gameSquare.appendChild(gameSquareCover);
+    // gameSquareCover.style.backgroundColor = 'green';
+    // gameSquareCover.style.height = '100px';
+
     shipPlacement[i][j] = 0;
     gameSquare.className = 'gameSquare';
     gameSquare.id = 'square' + j + i;
   }
 }
-var shipLength;
 
 function placeShip(shipLength) {
   var horizontal = Math.floor(Math.random() * 2) === 0;
@@ -40,7 +59,7 @@ function placeShip(shipLength) {
     for (var i = 0; i < shipLength + 1; i++) {
       var $square = $('#square' + (x+i) + (y));
       shipPlacement[x + i][y] = 1;
-      $square.css('backgroundColor', 'red');
+      // $square.css('backgroundColor', 'red');
     }
   }
   else {
@@ -54,7 +73,7 @@ function placeShip(shipLength) {
     for (var i = 0; i < shipLength + 1; i++) {
       var $square = $('#square' + (x) + (y + i));
       shipPlacement[x][y + i] = 1;
-      $square.css('backgroundColor', 'red');
+      // $square.css('backgroundColor', 'red');
     }
   }
 }
@@ -62,3 +81,4 @@ function placeShip(shipLength) {
 for (shipLength = 1; shipLength < 6; shipLength++) {
   placeShip(shipLength);
 }
+console.log(shipPlacement);
